a8ea388d8ec900f7589ee15a38e1ed24eb79a96e
Chunye Wang <chunye.wang@amd.com>
2025-04-13 04:00:41 -0500

    Refactor tar_main.cpp to improve output formatting and add tar_main.h (#61)
    Co-authored-by: Wang Chunye <chunywan@amd.com>

--- tar/src/tar_main.cpp
+++ tar/src/tar_main.cpp
@@ -6,6 +6,9 @@ extern "C" {
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+static std::string
+get_readable_path(const vaip_core::TarEntryInputStream& entry);
+
 int main(int argc, char* argv[]) {
   int opt = 0;
   int option_index = 0;
@@ -39,32 +42,39 @@ int main(int argc, char* argv[]) {
     }
     auto tar_file_obj = vaip_core::TarFile(std::move(tar_stream));
     auto& entries = tar_file_obj.entries();
-        std::cout << std::left // Align fields to the left
-              << std::setw(15) << "Size"           // Set width for "Size"
-              << std::setw(15) << "Block Begin"    // Set width for "Block Begin"
-              << std::setw(15) << "Block End"      // Set width for "Block End"
-              << std::setw(15) << "Data Begin"     // Set width for "Data Begin"
-              << std::setw(15) << "Data End"       // Set width for "Data End"
-              << std::setw(30) << "Path"           // Set width for "Path"
+    std::cout << std::left                      // Align fields to the left
+              << std::setw(15) << "Size"        // Set width for "Size"
+              << std::setw(15) << "Block Begin" // Set width for "Block Begin"
+              << std::setw(15) << "Block End"   // Set width for "Block End"
+              << std::setw(15) << "Data Begin"  // Set width for "Data Begin"
+              << std::setw(15) << "Data End"    // Set width for "Data End"
+              << std::setw(30) << "Path"        // Set width for "Path"
               << std::endl;
 
     for (const auto& entry : entries) {
-        std::cout << std::left // Align fields to the left
-                  << std::setw(15) << entry->size()              // File size
-                  << std::setw(15) << entry->block_begin_pos()   // Block begin position
-                  << std::setw(15) << entry->block_end_pos()     // Block end position
-                  << std::setw(15) << entry->data_begin_pos()    // Data begin position
-                  << std::setw(15) << entry->data_end_pos()      // Data end position
-                  << std::setw(30) << entry->path()              // File path
-                  << (!entry->is_symlink()
-                          ? std::string("")
-                          : std::string("->") + entry->real_path().value()) // Symlink info
-                  << std::endl;
+      auto name = get_readable_path(*entry);
+      std::cout << std::left                      // Align fields to the left
+                << std::setw(15) << entry->size() // File size
+                << std::setw(15)
+                << entry->block_begin_pos()       // Block begin position
+                << std::setw(15) << entry->block_end_pos() // Block end position
+                << std::setw(15)
+                << entry->data_begin_pos()                // Data begin position
+                << std::setw(15) << entry->data_end_pos() // Data end position
+                << std::setw(30)                          // File path
+                << name                                   // Symlink info
+                << std::endl;
     }
   } else {
     std::cerr << "Usage: " << argv[0] << " [-l] <file>" << std::endl;
     return 1;
   }
   return 0;
+}
+static std::string
+get_readable_path(const vaip_core::TarEntryInputStream& entry) {
+  return (entry.path() + (!entry.is_symlink() ? std::string("")
+                                              : std::string(" -> ") +
+                                                    entry.real_path().value()));
 }
 #include "getopt.c"
