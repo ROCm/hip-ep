dd6f20d73623a876d5a46c89bb9210a91aee5c4d
Chunye Wang <chunye.wang@amd.com>
2025-04-21 10:51:02 +0800

    add tools to build (#94)
    it simplifies the checker workflow.
    we use morphizen-demo for unit test and build. we don't need to build
    morphizen again.
    
    ---------
    
    Co-authored-by: Wang Chunye <chunywan@amd.com>

---
+++ tools/download_demo.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host "Download morphizen-demo ..."
+$ErrorActionPreference = "Stop"
+$morphizenDemoPath = "$Env:VAI_RT_WORKSPACE/morphizen-demo"
+if (-Not (Test-Path -Path $morphizenDemoPath)) {
+    Write-Host "morphizen-demo Directory does not exist. Cloning the repository..."
+    git clone git@gitenterprise.xilinx.com:VitisAI/morphizen-demo.git --branch dev --single-branch --depth 1 $morphizenDemoPath
+}
+Write-Host "morphizen-demo Directory has exist. Using the latest commit..."
+$currentDirectory = Get-Location
+Set-Location -Path $morphizenDemoPath
+git fetch --depth 1 origin dev
+git reset --hard FETCH_HEAD
+git clean -fdx
+Set-Location -Path $currentDirectory
+$HEAD = git rev-parse HEAD
+Write-Host "using morphizen-demo commit ID $HEAD"
---
+++ tools/download_ort.ps1
@@ -0,0 +1,36 @@
+﻿Write-Host "Download ONNXRuntime on Windows..."
+$ErrorActionPreference = "Stop"
+New-Item -Path $Env:VAI_RT_WORKSPACE -ItemType Directory -Force | Out-Null
+$directoryPath = "$Env:VAI_RT_WORKSPACE/onnxruntime"
+if (-Not (Test-Path -Path $directoryPath)) {
+    Write-Host "Directory does not exist. Cloning the repository..."
+    git clone https://github.com/Microsoft/onnxruntime.git --branch main --single-branch --depth 1 $directoryPath
+}
+else {
+    # Directory exists, skip cloning
+    Write-Host "Directory already exists. Skipping clone."
+}
+# Restore the directory to its original state
+# save the current dirctory
+$currentDirectory = Get-Location
+Set-Location -Path $directoryPath
+git fetch origin main
+$originalCommit = git rev-parse HEAD
+$wednesdayDate = (Get-Date).AddDays( - (7 + [int](Get-Date).DayOfWeek - 3) % 7).ToString("yyyy-MM-dd")
+Write-Host "Last Wednesday was $wednesdayDate"
+$firstCommit = git rev-list --reverse --since="$wednesdayDate 00:00" --until="$wednesdayDate 23:59" origin/main | Select-Object -First 1
+if ([string]::IsNullOrEmpty($firstCommit)) {
+    Write-Host "No need to update ONNXRuntime commit ID , no commits found on last Wednesday. "
+}
+elseif ($originalCommit -eq $firstCommit) {
+    Write-Host "No need to update ONNXRuntime commit ID ,  ORT commit ID is $originalCommit"
+}
+else {
+    Write-Host "Upgrade Onnxruntime commit ID from $originalCommit to $firstCommit "
+    git checkout $firstCommit --force
+}
+git clean -fdx
+$HEAD = git rev-parse HEAD
+Write-Host "using onnxruntime commit ID $HEAD"
+# restore to old directory
+Set-Location -Path $currentDirectory
