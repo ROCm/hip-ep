cfdd214a4f52ef3379778adbf3b4218da3bb8bd7
Chunye Wang <chunye.wang@amd.com>
2025-04-27 21:33:46 +0800

    fix complex cache issue (#126)
    Co-authored-by: Wang Chunye <chunywan@amd.com>

---
+++ tools/build_and_test.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = "Stop"
+$SCRIPT_DIR = $PSScriptRoot
+. "$SCRIPT_DIR/run-external-command.ps1"
+Get-ChildItem env:
+$env:CMAKE_PREFIX_PATH = "<prefix>;$env:CMAKE_PREFIX_PATH"
+# e.g., $env:CMAKE_PREFIX_PATH = "C:/mylibs;$env:CMAKE_PREFIX_PATH"
+Run cmake --version
+
+Run cmake -DCMAKE_FFIND_USE_PACKAGE_REGISTRY=OFF `
+    -DCMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY=ON `
+    -DBUILD_SHARED_LIBS=OFF `
+    "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>" `
+    -S "${Env:VAI_RT_WORKSPACE}/morphizen-demo" -B "$Env:VAI_RT_BUILD_DIR/morphizen-demo" `
+    --debug-find-pkg=glog `
+    "-DCMAKE_INSTALL_PREFIX=$Env:VAI_RT_PREFIX" `
+    "-DFETCHCONTENT_BASE_DIR=$Env:VAI_RT_PREFIX/morphizen_deps" `
+    --fresh
+
+$jobs = [Environment]::ProcessorCount
+
+Run cmake  --build  "$Env:VAI_RT_BUILD_DIR/morphizen-demo" -j $jobs --target install
+
+Run cmake  --build  "$Env:VAI_RT_BUILD_DIR/morphizen-demo" --target morphizen-run-all-unit-tests
+
+Run cmake  --build  "$Env:VAI_RT_BUILD_DIR/morphizen-demo" --target generate_ep_context_model
---
+++ tools/build_ort_and_deps.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = "Stop"
+$SCRIPT_DIR = $PSScriptRoot
+. "$SCRIPT_DIR/run-external-command.ps1"
+Set-Location "$Env:VAI_RT_WORKSPACE/onnxruntime"
+Run python tools/ci_build/build.py --use_vitisai --config Debug --build_shared_lib --parallel --compile_no_warning_as_error `
+    --skip_submodule_sync --build_dir "$Env:VAI_RT_BUILD_DIR/onnxruntime"   `
+    --skip_tests `
+    --enable_msvc_static_runtime `
+    --cmake_extra_defines `
+    "CMAKE_INSTALL_PREFIX=$Env:VAI_RT_PREFIX" `
+    onnxruntime_BUILD_UNIT_TESTS=OFF `
+    "CMAKE_CXX_FLAGS=/bigobj"
+Run cmake --build "$Env:VAI_RT_BUILD_DIR/onnxruntime/Debug" --target install
+Run python ../vai-rt/main.py --dev-mode --release_file=../vai-rt/release_file/latest_stx.txt --project zlib gsl json gtest protobuf glog
