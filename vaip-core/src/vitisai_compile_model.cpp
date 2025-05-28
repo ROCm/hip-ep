ROCm/hip-ep

commit 94055ff0ed9926e4163a2b4c3adb6e7c5db9b0d6

enable generic custom op for Non-NPU Device Compilation (#204)
# Summary of Changes

add `custom_op_generic` and fall back on cpu.

# Motivation

1. for offline compilation
2. for testing

---------

Co-authored-by: Wang Chunye <chunywan@amd.com>

vaip-core/src/vitisai_compile_model.cpp
@@ -992,7 +992,6 @@ create_execution_providers_from_ep_context_nodes(
       [](const vaip_cxx::NodeConstRef& a, const vaip_cxx::NodeConstRef& b) {
         return get_ep_context_index(a) < get_ep_context_index(b);
       });
-
   for (auto idx = 0u; idx < size; ++idx) {
     auto node = ep_context_nodes[idx];
     auto index = get_ep_context_index(node);
@@ -1038,7 +1037,44 @@ static void dirty_hack_for_model_clone_external_data_threshold(
     }
   }
 }
-
+static bool is_compiling_on_non_npu_platform(PassContextImp& context) {
+  auto is_ep_context_enabled =
+      context.get_session_config(kOrtSessionOptionEpContextEnable, "0") == "1";
+  if (!is_ep_context_enabled) {
+    // if EP context is not enabled, it is not a compilation flow.
+    return false;
+  }
+  auto enable_generic_custom_op =
+      context.get_provider_option("XLNX_enable_generic_custom_op", "0") != "0";
+  if (context.get_session_config(kOrtSessionOptionsDisableModelCompile, "1") ==
+      "0") {
+    enable_generic_custom_op = true; // see also MicroSoft/Onnxruntime#24416
+  }
+  if (enable_generic_custom_op) {
+    return true;
+  }
+  auto xrt_coreutil_dll = vaip_core::Plugin::get("xrt_coreutil");
+  if (xrt_coreutil_dll == nullptr) {
+    // when xrt is not installed, we assume it is on non-npu platform.
+    return true;
+  }
+  auto xrt_device_open =
+      xrt_coreutil_dll->get_method<void*, int>("xrtDeviceOpen");
+  auto xrt_device_close =
+      xrt_coreutil_dll->get_method<int, void*>("xrtDeviceClose");
+  if (xrt_device_open == nullptr || xrt_device_close == nullptr) {
+    // when wrong version of xrt is installed, it won't work, we also assume
+    // there is no NPU.
+    return true;
+  }
+  auto device_id = xrt_device_open(0);
+  auto cannot_open_npu_device = device_id == nullptr;
+  xrt_device_close(device_id);
+  if (cannot_open_npu_device) {
+    return true;
+  }
+  return false;
+}
 static std::vector<std::unique_ptr<ExecutionProvider>>
 compile_onnx_model_internal(
     const Graph& onnx_graph,
@@ -1075,8 +1111,17 @@ compile_onnx_model_internal(
     measure_after_compile_onnx_model_2 =
         context->measure("after_compile_onnx_model_internal");
     ret.reserve(context->context_proto.meta_def_size());
-    for (auto& meta_def : context->context_proto.meta_def()) {
-      auto& device = meta_def.device();
+    auto enable_generic_custom_op = is_compiling_on_non_npu_platform(*context);
+    if (enable_generic_custom_op) {
+      LOG(INFO) << "detect running on Non-NPU platform, compilation only";
+    }
+    for (auto& meta_def : *context->context_proto.mutable_meta_def()) {
+      std::string device = meta_def.device();
+      if (enable_generic_custom_op) {
+        // ovewrite default device for offline compilation flow.
+        device = "GENERIC";
+        meta_def.set_fallback_cpu(true);
+      }
       auto plugin_name = std::string("vaip_custom_op_") + device;
       ret.emplace_back(
           ExecutionProviderConcrete::create(plugin_name, context, meta_def));

CMakeLists.txt
@@ -30,7 +30,10 @@ if(morphizen_ENABLE_MORPHIZEN_CORE_DYNAMIC)
   add_subdirectory (pattern-gen)
   add_subdirectory (onnx-grep)
 endif(morphizen_ENABLE_MORPHIZEN_CORE_DYNAMIC)
-  add_subdirectory (vaip_pass_init)
+
+add_subdirectory (vaip_pass_init)
+add_subdirectory (custom-op-generic)
+
 if(morphizen_ENABLE_UNIT_TEST AND morphizen_ENABLE_MORPHIZEN_CORE_DYNAMIC)
   # when morphzen-core-dyanmic is enabled, we can build unit-test, otherwise we will get link errors.
   include(CTest)

custom-op-generic/CMakeLists.txt
@@ -1,0 +1,27 @@
+##
+# ** Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All rights reserved.
+# ** Licensed under the MIT License.
+##
+
+project(
+  morphizen-custom-op-generic
+  VERSION 1.0.0
+  LANGUAGES C CXX)
+set(LIB_NAME "morphizen-custom-op-generic")
+add_library(
+  ${LIB_NAME} STATIC
+  src/custom_op_generic.hpp
+  src/custom_op_generic.cpp
+  src/custom_op_generic_main.cpp
+)
+find_package (glog CONFIG REQUIRED)
+set(LIB_NAME morphizen-custom-op-generic)
+set_target_properties(${LIB_NAME} PROPERTIES FOLDER "morphizen/custom-ops")
+target_link_libraries(${LIB_NAME} PRIVATE
+  glog::glog
+  morphizen::morphizen-core-static
+  )
+
+set_target_properties(${LIB_NAME} PROPERTIES
+  OUTPUT_NAME "${LIB_NAME}")
+target_link_libraries(${morphizen_ONNXRUNTIME_VITISAI_EP_TARGET} PRIVATE "$<LINK_LIBRARY:WHOLE_ARCHIVE,${LIB_NAME}>")

custom-op-generic/src/custom_op_generic.cpp
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Licensed under the MIT License.
+ */
+#pragma once
+#include "./custom_op_generic.hpp"
+#include "morphizen/onnxruntime_api.hpp"
+#include <glog/logging.h>
+#include <morphizen/env_config.hpp>
+
+DEF_ENV_PARAM(MORPHIZEN_DEBUG_CUSTOM_OP_GENERIC, "0")
+#define MY_LOG(n)                                                              \
+  LOG_IF(INFO, ENV_PARAM(MORPHIZEN_DEBUG_CUSTOM_OP_GENERIC) >= n)
+
+namespace morphizen {
+namespace custom_op_generic {
+using namespace vaip_core;
+MyCustomOp::MyCustomOp(std::shared_ptr<const PassContext> context,
+                       const std::shared_ptr<MetaDefProto>& meta_def,
+                       onnxruntime::Model* model)
+    : CustomOpImp(context, meta_def, model) {
+  MY_LOG(1) << "MyCustomOp ctor: ";
+}
+MyCustomOp::~MyCustomOp() { MY_LOG(1) << "MyCustomOp dtor: "; }
+void MyCustomOp::Compute(const OrtApi* api, OrtKernelContext* context) const {
+  this->ComputeCpu(api, context);
+}
+} // namespace custom_op_generic
+} // namespace morphizen

custom-op-generic/src/custom_op_generic.hpp
@@ -1,0 +1,26 @@
+/*
+ * Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Licensed under the MIT License.
+ */
+#pragma once
+#define VAIP_CUSTOM_OP // TODO: remove it
+#include "morphizen/vaip.hpp"
+
+namespace morphizen {
+namespace custom_op_generic {
+using namespace vaip_core;
+
+class MyCustomOp : public ::vaip_core::CustomOpImp {
+public:
+  MyCustomOp(std::shared_ptr<const PassContext> context,
+             const std::shared_ptr<MetaDefProto>& meta_def,
+             onnxruntime::Model* model);
+
+  virtual ~MyCustomOp();
+
+private:
+  virtual void Compute(const OrtApi* api,
+                       OrtKernelContext* context) const override final;
+};
+} // namespace custom_op_generic
+} // namespace morphizen

custom-op-generic/src/custom_op_generic_main.cpp
@@ -1,0 +1,23 @@
+/*
+ * Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+#include <glog/logging.h>
+
+#include "./custom_op_generic.hpp"
+#include "morphizen/vaip.hpp"
+
+static vaip_core::ExecutionProvider* create_execution_provider_imp(
+    std::shared_ptr<const vaip_core::PassContext>& context,
+    const vaip_core::MetaDefProto& meta_def) {
+  return new vaip_core::ExecutionProviderImp<
+      morphizen::custom_op_generic::MyCustomOp>(context, meta_def);
+}
+
+namespace {
+// TODO: do we need to rename ::vaip_core to ::morphizen?
+static ::vaip_core::StaticPluginRegister
+    __register("vaip_custom_op_GENERIC", "create_execution_provider",
+               (void*)&create_execution_provider_imp);
+} // namespace

vaip-core/src/pass_context_imp.cpp
@@ -729,6 +729,20 @@ void PassContextImp::save_context_json() const {
                   "context.json") == proto.mutable_cache_files()->end()) {
       proto.add_cache_files("context.json");
     }
+    // When the GENERIC device is used, set fallback_cpu to true. When
+    // inferencing a cached model, either from the cache directory or the EP
+    // cache context file, we should not enable fallback_cpu. Otherwise, a
+    // considerable amount of overhead is incurred for creating an
+    // `Ort::Session` object for the subgraph behind the scenes.
+
+    // There is a pitfall: if a custom op really needs to fall back to the CPU,
+    // and the GENERIC device is enabled for model compilation, it is a bug.
+    // However, this combination is not in used for now. We can fix it later.
+    for (auto& meta_def : *proto.mutable_meta_def()) {
+      if (meta_def.device() == "GENERIC") {
+        meta_def.set_fallback_cpu(true);
+      }
+    }
     auto json_str = msg_to_json_string(proto);
     const_cast<PassContextImp*>(this)->write_file("context.json", json_str);
   } catch (const std::exception& e) {

commit 6404eb08b480a043e83be6c369c08788385d9d7d

fix compilation error for vaip (#236)
# Summary of Changes

fix compilation error for vaip build, build error was introduced by #204

see PR VitisAI/vaip#5018 also

# Motivation

```
vaip-core/src/vitisai_compile_model.cpp:1049:34: error: ‘kOrtSessionOptionsDisableModelCompile’ was not declared in this scope; did you mean ‘kOrtSessionOptionsDisableQuantQDQ’?
 1049 |   if (context.get_session_config(kOrtSessionOptionsDisableModelCompile, "1") ==
      |                                  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      |                                  kOrtSessionOptionsDisableQuantQDQ
```

# Implementation

not to use `kOrtSessionOptionsDisableModelCompile`, it was introduced by
https://github.com/microsoft/onnxruntime/pull/24416

---------

Co-authored-by: Wang Chunye <chunywan@amd.com>

.gitignore
@@ -37,3 +37,4 @@
 /build.ninja/
 /compile_commands.json
 /tools/__pycache__/
+CMakePresets.json

vaip-core/src/vitisai_compile_model.cpp
@@ -1046,8 +1046,11 @@ static bool is_compiling_on_non_npu_platform(PassContextImp& context) {
   }
   auto enable_generic_custom_op =
       context.get_provider_option("XLNX_enable_generic_custom_op", "0") != "0";
-  if (context.get_session_config(kOrtSessionOptionsDisableModelCompile, "1") ==
-      "0") {
+  // TODO: vai-rt need to upgrade onnxruntime
+  static const char* kOrtSessionOptionsDisableModelCompile_local =
+      "session.disable_model_compile";
+  if (context.get_session_config(kOrtSessionOptionsDisableModelCompile_local,
+                                 "1") == "0") {
     enable_generic_custom_op = true; // see also MicroSoft/Onnxruntime#24416
   }
   if (enable_generic_custom_op) {