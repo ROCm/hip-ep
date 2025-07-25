d03d747ce77779e9fe1baa13f8189011fcc90e00
Meng, Tianfang <tianfang.meng@amd.com>
2025-07-25 20:21:19 +0800

    Add cmake option for trace/profile (#308)
    <!--
    Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All rights
    reserved.
    Licensed under the MIT License.
    -->
    # Summary of Changes
    
    * At a high-level, describe this request adds/changes/improves/fixes
    * You don't need to describe every change but each request should have
    an overarching theme
    * Don't mix a lot of big unrelated modifications in one request
    
    Closes #x (each request should address an open issue. Open an issue
    first to discuss what you'd like to change.)
    
    # Motivation
    
    Describe why this request is needed/good/useful etc.
    If this information is already captured in detail in the related issue,
    just provide a high-level statement and link to the issue.
    
    # Implementation
    
    While your code changes can be seen in the diff between branches, it may
    not capture why certain choices were made, what else had had been tried
    unsuccessfully etc.
    
    ---------
    
    Co-authored-by: Andrew Luo <andrew.luo@amd.com>
    Co-authored-by: Wang Chunye <chunywan@amd.com>

--- vaip-core/src/vaip_profile.cpp
+++ vaip-core/src/vaip_profile.cpp
@@ -4,12 +4,10 @@
  */
 
 #include <glog/logging.h>
-#if USE_VART_TRACE
-#  include <vart/trace/event.hpp>
-#  include <vart/trace/trace.hpp>
-#  include <vitis/ai/profiling.hpp>
-#endif
-#include "morphizen/vaip_ort.hpp"
+#include <morphizen-utils/morphizen-utils.hpp>
+DEF_ENV_PARAM(MORPHIZEN_DEBUG_PROFILER_COLLECT, "0")
+#define MY_LOG(n) LOG_IF(INFO, ENV_PARAM(MORPHIZEN_DEBUG_PROFILER_COLLECT) >= n)
+#include <morphizen/vaip.hpp>
 
 namespace vaip_core {
 
@@ -18,41 +16,21 @@ namespace vaip_core {
  *
  * Return trace data to onnxruntime.
  */
-VAIP_DLL_SPEC void
-profiler_collect([[maybe_unused]] std::vector<EventInfo>& api_events,
-                 [[maybe_unused]] std::vector<EventInfo>& kernel_events) {
+typedef void (*profiler_collect_t)(std::vector<EventInfo>& api_events,
+                                   std::vector<EventInfo>& kernel_events);
 
-#if USE_VART_TRACE
-  uint64_t start_time = 0;
-  uint64_t stop_time = 0;
-  std::unordered_map<std::string, std::string> event_args;
+VAIP_DLL_SPEC void profiler_collect(std::vector<EventInfo>& api_events,
+                                    std::vector<EventInfo>& kernel_events) {
 
-  auto ret1 = vitis::ai::trace::trace_collect();
-  auto ret2 = vitis::ai::profiling::profiling_collect();
+  auto profiler_collect_ptrs =
+      vaip_core::Plugin::get_all_symbols("profiler_collect_real");
 
-  /* Return trace data from add_trace() into onnxruntime events */
-  for (auto& entry : ret1) {
-
-    if (entry[std::string("classname")] == "user-task") {
-
-      if (entry["event_state"] == "1") {
-        start_time = stoll(entry["ts"]);
-      } else if (entry["event_state"] == "0") {
-        stop_time = stoll(entry["ts"]);
-
-        api_events.emplace_back(std::make_tuple(
-            entry["event_name"], stoi(entry["pid"]), stoi(entry["tid"]),
-            start_time, stop_time - start_time));
-      }
-    }
+  for (const auto& profiler_collect_ptr : profiler_collect_ptrs) {
+    MY_LOG(1) << " running " << profiler_collect_ptr.first
+              << "::profiler_collect_real";
+    auto profiler_collect_func =
+        reinterpret_cast<profiler_collect_t>(profiler_collect_ptr.second);
+    profiler_collect_func(api_events, kernel_events);
   }
-
-  /* Return trace data from __TIC__/__TOC__ into onnxruntime events */
-  for (auto& entry : ret2) {
-    api_events.emplace_back(std::make_tuple(entry.tag, entry.pid, entry.tid,
-                                            entry.timestamp, entry.duration));
-  }
-#endif
 }
-
 } // namespace vaip_core
