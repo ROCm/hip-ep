0eef2521b290fdf6db25193ab96ab8d0b3b6ffac
Klein, Lauretta <lauretta.klein@amd.com>
2025-11-06 11:57:06 +0000

    Use hashlib find_package target if found (#441)
    <!--
    Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All rights
    reserved.
    Licensed under the MIT License.
    -->
    # Summary of Changes
    
    * also add using the hashlib target from find_package if package is
    found
    
    # Motivation
    
    This is being used in the monorepo already by a patch - we just want to
    "upstream" this change to avoid maintaining the patch.
    
    # Implementation
    
    While your code changes can be seen in the diff between branches, it may
    not capture why certain choices were made, what else had had been tried
    unsuccessfully etc.

--- vaip-core/cmake/hash-library.cmake
+++ vaip-core/cmake/hash-library.cmake
@@ -3,21 +3,26 @@
 # ** Licensed under the MIT License.
 ##
 
-add_library(HASH_LIBRARY OBJECT
-  ../3rd-party/hash-library/crc32.cpp
-  ../3rd-party/hash-library/crc32.h
-  ../3rd-party/hash-library/hash.h
-  ../3rd-party/hash-library/hmac.h
-  ../3rd-party/hash-library/keccak.cpp
-  ../3rd-party/hash-library/keccak.h
-  ../3rd-party/hash-library/md5.cpp
-  ../3rd-party/hash-library/md5.h
-  ../3rd-party/hash-library/sha1.cpp
-  ../3rd-party/hash-library/sha1.h
-  ../3rd-party/hash-library/sha256.cpp
-  ../3rd-party/hash-library/sha256.h
-  ../3rd-party/hash-library/sha3.cpp
-  ../3rd-party/hash-library/sha3.h)
+find_package(hashlib CONFIG QUIET)
+if(NOT TARGET hashlib::hashlib)
+  add_library(HASH_LIBRARY OBJECT
+    ../3rd-party/hash-library/crc32.cpp
+    ../3rd-party/hash-library/crc32.h
+    ../3rd-party/hash-library/hash.h
+    ../3rd-party/hash-library/hmac.h
+    ../3rd-party/hash-library/keccak.cpp
+    ../3rd-party/hash-library/keccak.h
+    ../3rd-party/hash-library/md5.cpp
+    ../3rd-party/hash-library/md5.h
+    ../3rd-party/hash-library/sha1.cpp
+    ../3rd-party/hash-library/sha1.h
+    ../3rd-party/hash-library/sha256.cpp
+    ../3rd-party/hash-library/sha256.h
+    ../3rd-party/hash-library/sha3.cpp
+    ../3rd-party/hash-library/sha3.h)
 
-target_link_libraries(morphizen-core-static PRIVATE HASH_LIBRARY)
-set_target_properties(HASH_LIBRARY PROPERTIES FOLDER Dependencies/hash-library)
+  target_link_libraries(morphizen-core-static PRIVATE HASH_LIBRARY)
+  set_target_properties(HASH_LIBRARY PROPERTIES FOLDER Dependencies/hash-library)
+else()
+  target_link_libraries(morphizen-core-static PRIVATE hashlib::hashlib)
+endif()
