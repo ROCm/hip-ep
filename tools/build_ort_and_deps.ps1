660f9cf16fdc9bdd80c1ac349e67351c91913409
iebuild, z1 <z1.iebuild@amd.com>
2025-11-24 10:22:48 +0800

    Update build_ort_and_deps.ps1

--- tools/build_ort_and_deps.ps1
+++ tools/build_ort_and_deps.ps1
@@ -14,5 +14,7 @@ Run python tools/ci_build/build.py --use_vitisai --config Debug --build_shared_l
     "CMAKE_INSTALL_PREFIX=$Env:VAI_RT_PREFIX" `
     onnxruntime_BUILD_UNIT_TESTS=OFF `
     "CMAKE_CXX_FLAGS=/bigobj"
+Run python -c "import sys; print('PYTHON USED:', sys.executable)"
+Run python -m pip  install requests
 Run cmake --build "$Env:VAI_RT_BUILD_DIR/onnxruntime/Debug" --target install
 Run python ../vai-rt/main.py --dev-mode --release_file=../vai-rt/release_file/latest_stx.txt --project zlib gsl gtest protobuf glog
