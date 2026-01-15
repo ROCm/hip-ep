ed62ac41a4f243f71434eb22fe0b895b9ff25526
Zhong, GenMing <GenMing.Zhong@amd.com>
2026-01-14 23:59:23 -0600

    [AIESW-22106] update log while xclbin doesn't exist (#498)
    <!--
    Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All rights
    reserved.
    Licensed under the MIT License.
    -->
    # Summary of Changes
    https://jira.xilinx.com/browse/AIESW-22106
    When the xclbin does not exist, do not print
    ```
    Xclbin path doesn't exist, are you running with cpu  runner? Path: 
    ```
    # Motivation
    
    The user complained that this log message could be misleading. However,
    developers sometimes need this log for troubleshooting, so a printing
    condition LOG_VERBOSE=1 was added to this log line.
    
    # Implementation
    
    While your code changes can be seen in the diff between branches, it may
    not capture why certain choices were made, what else had had been tried
    unsuccessfully etc.
    
    ---------
    
    Co-authored-by: genmingz <genmingz@amd.com>

--- vaip-core/src/pass_context_imp.cpp
+++ vaip-core/src/pass_context_imp.cpp
@@ -822,9 +822,9 @@ std::filesystem::path PassContextImp::xclbin_path_to_cache_files(
     std::vector<char> buffer = read_file_to_buffer(path);
     const_cast<PassContextImp*>(this)->write_file(filename, buffer);
   } else {
-    LOG(INFO) << "Xclbin path doesn't exist, are you running with cpu "
-                 "runner? Path: "
-              << path.string();
+    LOG_VERBOSE(2) << "Xclbin path doesn't exist, are you running with cpu "
+                      "runner? Path: "
+                   << path.string();
     return path;
   }
   return ret;
