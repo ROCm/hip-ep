5eb2093359eecffcdd054c1bffac2dd43a04e8c4
Wang Chunye <chunywan@amd.com>
2026-01-15 22:32:43 -0800

    Fix pattern generator cmake files to use morphizen xxd.py tool

--- level-2-pass-rocm-gemm/cmake/generate_pattern_inc.cmake
+++ level-2-pass-rocm-gemm/cmake/generate_pattern_inc.cmake
@@ -2,10 +2,14 @@
 # ** Copyright (C) 2023 - 2026 Advanced Micro Devices, Inc. All rights reserved.
 # ** Licensed under the MIT License.
 ##
-file(READ "${CMAKE_CURRENT_SOURCE_DIR}/../patterns/gemm.json" JSON_CONTENT)
-string(REPLACE "\"" "\\\"" JSON_CONTENT_ESCAPED ${JSON_CONTENT})
-string(REPLACE "\n" "\\n" JSON_CONTENT_ESCAPED ${JSON_CONTENT_ESCAPED})
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/gemm_json.inc"
-  "static const unsigned char gemm_json[] = \"${JSON_CONTENT_ESCAPED}\";\n"
-  "static const unsigned int gemm_json_len = sizeof(gemm_json) - 1;\n"
+find_package(Python3 COMPONENTS Interpreter REQUIRED)
+add_custom_command (
+  OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/gemm_pattern_json.hpp
+  COMMAND ${CMAKE_COMMAND} -E env
+    $<TARGET_FILE:Python3::Interpreter> ${morphizen_SOURCE_DIR}/tools/xxd.py
+    "--column" 16
+    "--var" gemm_json
+    --output ${CMAKE_CURRENT_BINARY_DIR}/gemm_pattern_json.hpp
+    "${CMAKE_CURRENT_SOURCE_DIR}/../patterns/gemm.json"
+    DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/../patterns/gemm.json" "${CMAKE_CURRENT_LIST_FILE}"
 )
