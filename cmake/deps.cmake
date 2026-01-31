404d7cb48eded5d8864bcdbeea16d25dfd6ce18c
Ming Wang <m.wang@amd.com>
2026-01-30 19:05:01 -0600

    update readme

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -60,8 +60,8 @@ set(LLVM_ENABLE_ZSTD OFF CACHE BOOL "Enable zstd compression")
 
 
 # Try to find pre-installed LLVM/MLIR first
-find_package(LLVM QUIET CONFIG)
-find_package(MLIR QUIET CONFIG)
+#find_package(LLVM QUIET CONFIG)
+#find_package(MLIR QUIET CONFIG)
 
 if(LLVM_FOUND AND MLIR_FOUND)
   message(STATUS "Found pre-installed LLVM and MLIR")
