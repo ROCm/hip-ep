5d1f5d5dca103fa5a1deadb1d21492cfb831ca4d
zupeiye2 <zupeiye2@amd.com>
2026-02-05 07:56:20 -0600

    linrunner format

--- custom-op-rocm/src/main.cpp
+++ custom-op-rocm/src/main.cpp
@@ -1,22 +1,25 @@
-// Copyright (C) 2023 - 2026 Advanced Micro Devices, Inc. All rights reserved.
-// Licensed under the MIT License.
+/*
+ * Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
-#include <glog/logging.h>
-#include "morphizen/morphizen.hpp"
 #include "custom_op.hpp"
+#include "morphizen/morphizen.hpp"
+#include <glog/logging.h>
 
 /**
  * Custom Op Registration Entry Point
- * 
+ *
  * This file registers the ROCm custom op with MorphiZen.
- * The plugin name must match what's in morphizen_config.json execution_providers section.
+ * The plugin name must match what's in morphizen_config.json
+ * execution_providers section.
  */
 
-static morphizen::ExecutionProvider* create_execution_provider_imp(
-    std::shared_ptr<const morphizen::PassContext>& context,
-    const morphizen::MetaDefProto& meta_def) {
-  return new morphizen::ExecutionProviderImp<hip_ep::RocmCustomOp>(
-      context, meta_def);
+static morphizen::ExecutionProvider *create_execution_provider_imp(
+    std::shared_ptr<const morphizen::PassContext> &context,
+    const morphizen::MetaDefProto &meta_def) {
+  return new morphizen::ExecutionProviderImp<hip_ep::RocmCustomOp>(context,
+                                                                    meta_def);
 }
 
 namespace {
@@ -25,5 +28,5 @@ namespace {
 // is the device passed to try_fuse() in the pass
 static ::morphizen::StaticPluginRegister
     __register("morphizen_custom_op_ROCm_EP", "create_execution_provider",
-               (void*)&create_execution_provider_imp);
-}  // namespace
+               (void *)&create_execution_provider_imp);
+} // namespace
