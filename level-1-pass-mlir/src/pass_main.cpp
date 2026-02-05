f3ac9968f2ade840a0eddeaf2e57582f84334e8e
zhenzew <zhenzew@amd.com>
2026-02-04 21:40:34 -0600

    feat: update MorphiZen to support bytecode format (PR #112)

--- level-1-pass-mlir/src/pass_main.cpp
+++ level-1-pass-mlir/src/pass_main.cpp
@@ -7,11 +7,13 @@
 #include <glog/logging.h>
 
 // MLIR includes
+#include "mlir/Bytecode/BytecodeReader.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/Parser/Parser.h"
+#include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -30,20 +32,32 @@ struct Level1MlirPass {
 
     auto graph_ref = GraphConstRef(graph);
     auto graph_string = graph_ref.save_string();
-    LOG(INFO) << "Graph serialized to string, size: " << graph_string->size();
+    LOG(INFO) << "Graph serialized to bytecode, size: " << graph_string->size();
 
-    // Parse MLIR string to mlir::ModuleOp
-    LOG(INFO) << "Parsing MLIR string to ModuleOp...";
+    // Parse MLIR bytecode to mlir::ModuleOp
+    LOG(INFO) << "Parsing MLIR bytecode to ModuleOp...";
     mlir::MLIRContext context;
     context.loadDialect<mlir::func::FuncDialect>();
     context.loadDialect<mlir::arith::ArithDialect>();
     context.allowUnregisteredDialects();
 
+    // Create a MemoryBuffer from the bytecode data
+    auto memBuffer = llvm::MemoryBuffer::getMemBuffer(
+        llvm::StringRef(graph_string->data(), graph_string->size()),
+        "mlir-bytecode",
+        /*RequiresNullTerminator=*/false);
+
+    // Create SourceMgr and add the buffer
+    llvm::SourceMgr sourceMgr;
+    sourceMgr.AddNewSourceBuffer(std::move(memBuffer), llvm::SMLoc());
+
+    // Parse bytecode using parseSourceFile with SourceMgr
+    mlir::ParserConfig parserConfig(&context);
     auto moduleRef =
-        mlir::parseSourceString<mlir::ModuleOp>(*graph_string, &context);
+        mlir::parseSourceFile<mlir::ModuleOp>(sourceMgr, parserConfig);
 
     if (!moduleRef) {
-      LOG(INFO) << "Failed to parse MLIR string to ModuleOp";
+      LOG(INFO) << "Failed to parse MLIR bytecode to ModuleOp";
     } else {
       // Get the module operation
       mlir::ModuleOp module = *moduleRef;
