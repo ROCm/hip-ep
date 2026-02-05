1db1ec67d649331404cd95c54ed917cedfe1a4a8
zz002 <zhenzew@amd.com>
2026-02-05 04:53:20 -0600

    Disable pattern matching log output (#132)
    Change LOG(INFO) to LOG_IF(INFO, false) in pattern_log.hpp to disable
    verbose pattern matching logs.

--- morphizen-pattern/src/pattern_log.hpp
+++ morphizen-pattern/src/pattern_log.hpp
@@ -9,7 +9,7 @@
 
 // Simple logging for pattern matching (debug level can be controlled via glog
 // flags)
-#define MY_LOG(n) LOG(INFO)
+#define MY_LOG(n) LOG_IF(INFO, false)
 #define MATCH_FAILED MY_LOG(1) << "MATCH FAILED. ID=" << get_id() << ";"
 namespace morphizen {
 [[maybe_unused]] static std::string node_input_as_string(const Graph& graph,
