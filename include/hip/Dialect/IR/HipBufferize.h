041bc1ea9ddd36afd7b34e2af3ef8e0581048ea3
genmingz <genmingz@amd.com>
2026-04-16 03:47:15 -0500

    fix runtime error

--- include/hip/Dialect/IR/HipBufferize.h
+++ include/hip/Dialect/IR/HipBufferize.h
@@ -89,6 +89,8 @@ registerHipBufferizableOpInterfaceModels(DialectRegistry &registry) {
     HipDNNGraphOp::attachInterface<HipDstBufferizableModel<HipDNNGraphOp>>(
         *ctx);
     GemmOp::attachInterface<HipDstBufferizableModel<GemmOp>>(*ctx);
+    LinearAttentionOp::attachInterface<
+        HipDstBufferizableModel<LinearAttentionOp>>(*ctx);
   });
 }
 
