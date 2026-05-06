c793c49bf858f3a8acb4f7d2445f5d0fda2e69ff
genmingz <genmingz@amd.com>
2026-05-06 02:42:08 -0500

    lint

--- lib/Conversion/HipToLLVM/RangeLowering.cpp
+++ lib/Conversion/HipToLLVM/RangeLowering.cpp
@@ -49,10 +49,14 @@ struct RangeOpLowering : public ConvertOpToLLVMPattern<RangeOp> {
                                          "unsupported element type for range");
 
     Value statePtr = adaptor.getCtx();
-    Value startPtr = extractContiguousMemRefPtr(adaptor.getStart(), rewriter, loc);
-    Value limitPtr = extractContiguousMemRefPtr(adaptor.getLimit(), rewriter, loc);
-    Value deltaPtr = extractContiguousMemRefPtr(adaptor.getDelta(), rewriter, loc);
-    Value outputPtr = extractContiguousMemRefPtr(adaptor.getOutput(), rewriter, loc);
+    Value startPtr =
+        extractContiguousMemRefPtr(adaptor.getStart(), rewriter, loc);
+    Value limitPtr =
+        extractContiguousMemRefPtr(adaptor.getLimit(), rewriter, loc);
+    Value deltaPtr =
+        extractContiguousMemRefPtr(adaptor.getDelta(), rewriter, loc);
+    Value outputPtr =
+        extractContiguousMemRefPtr(adaptor.getOutput(), rewriter, loc);
     Value outputNumElems =
         computeNumElements(outputType, adaptor.getOutput(), rewriter, loc);
 
