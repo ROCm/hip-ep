d75083b02c6696fe1088a5ba661d35bb85c235d0
zhenzew <zhenzew@amd.com>
2026-07-27 02:02:00 -0500

    feat(hipsr): register no-op hipsr-pool-alloc pass skeleton

---
+++ lib/Dialect/Hipsr/Transforms/HipsrPoolAllocPass.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+#include "hip/Dialect/Hipsr/Transforms/Passes.h"
+
+#include "hip/Dialect/Hipsr/IR/HipsrDialect.h"
+
+#include "mlir/IR/BuiltinOps.h"
+
+namespace mlir {
+namespace hipsr {
+
+#define GEN_PASS_DEF_HIPSRPOOLALLOCPASS
+#include "hip/Dialect/Hipsr/Transforms/Passes.h.inc"
+
+namespace {
+
+struct HipsrPoolAllocPass
+    : impl::HipsrPoolAllocPassBase<HipsrPoolAllocPass> {
+  using impl::HipsrPoolAllocPassBase<
+      HipsrPoolAllocPass>::HipsrPoolAllocPassBase;
+
+  void runOnOperation() override {}
+};
+
+} // namespace
+
+} // namespace hipsr
+} // namespace mlir
