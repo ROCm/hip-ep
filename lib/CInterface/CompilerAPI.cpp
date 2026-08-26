d4dbaf1d77169ea21ac18e890ca305a2c346aa62
genmingz <genmingz@amd.com>
2026-08-25 22:14:21 -0500

    BUILD_EP=ON: build static lib BUILD_EP=OFF: build dynamic lib

--- lib/CInterface/CompilerAPI.cpp
+++ lib/CInterface/CompilerAPI.cpp
@@ -9,7 +9,10 @@
 #include "hip/flatbuffers_json.h"
 
 #include "morphizen-foundation/file_io.hpp"
+
+#ifdef HIP_COMPILER_STATIC
 #include "morphizen-utils/morphizen_plugin.hpp"
+#endif
 
 #include "compilation_options_schema.h"
 
@@ -17,6 +20,7 @@
 
 #include <cstring>
 #include <string>
+
 #ifdef HIPDNN_GRAPH_RUNTIME_AVAILABLE
 #include "../HipDNNGraphRuntime/hipdnn_graph_runtime.h"
 #endif
@@ -133,12 +137,16 @@ COMPILER_API const char *hip_get_version(void) { return COMPILER_VERSION; }
 
 } // extern "C"
 
-namespace{
+#ifdef HIP_COMPILER_STATIC
+namespace {
+
 static ::morphizen::StaticPluginRegister
     __register_hip_compile("hip-compiler", "hip_compile_with_fs",
-                           reinterpret_cast<void *>(&hip_compile_with_fs));
+                         reinterpret_cast<void *>(&hip_compile_with_fs));
 
 static ::morphizen::StaticPluginRegister
     __register_hip_version("hip-compiler", "hip_get_version",
-                           reinterpret_cast<void *>(&hip_get_version));
-}
+                         reinterpret_cast<void *>(&hip_get_version));
+
+} // namespace
+#endif
