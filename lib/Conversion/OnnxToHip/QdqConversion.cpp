b1aa1310f976138255141be574d2c84028feb522
genmingz <genmingz@amd.com>
2026-08-31 21:58:50 -0500

    lint

--- lib/Conversion/OnnxToHip/QdqConversion.cpp
+++ lib/Conversion/OnnxToHip/QdqConversion.cpp
@@ -3,11 +3,11 @@
  * Licensed under the MIT License.
  */
 
- #include "OnnxToHipUtils.h"
+#include "OnnxToHipUtils.h"
 
- namespace mlir {
- namespace hip {
- namespace {
+namespace mlir {
+namespace hip {
+namespace {
 static mlir::Value getOptionalOperand(mlir::Operation *op, size_t idx) {
   if (idx >= op->getNumOperands())
     return nullptr;
@@ -18,55 +18,58 @@ static mlir::Value getOptionalOperand(mlir::Operation *op, size_t idx) {
 }
 
 struct QuantizeLinearDecompose : public mlir::RewritePattern {
-    QuantizeLinearDecompose(mlir::MLIRContext *ctx)
-    : RewritePattern("onnx.QuantizeLinear", /*benefit=*/1, ctx) {}
+  QuantizeLinearDecompose(mlir::MLIRContext *ctx)
+      : RewritePattern("onnx.QuantizeLinear", /*benefit=*/1, ctx) {}
 
-    mlir::LogicalResult
+  mlir::LogicalResult
   matchAndRewrite(mlir::Operation *op,
                   mlir::PatternRewriter &rewriter) const override {
     // check operands and results. should be 2/3 input and 1 output.
-    bool operandsValid = op->getNumOperands()==2 || op->getNumOperands()==3;
+    bool operandsValid = op->getNumOperands() == 2 || op->getNumOperands() == 3;
     if (!operandsValid || op->getNumResults() != 1)
-      return rewriter.notifyMatchFailure(op, "expected 2/3 inputs and 1 output");
-    mlir::Value input = op->getOperand(0); 
-    mlir::Value scale = op->getOperand(1); 
-    mlir::Value zero_point = getOptionalOperand(op, 2); 
-auto resultType = mlir::dyn_cast<mlir::RankedTensorType>(op->getResult(0).getType());
+      return rewriter.notifyMatchFailure(op,
+                                         "expected 2/3 inputs and 1 output");
+    mlir::Value input = op->getOperand(0);
+    mlir::Value scale = op->getOperand(1);
+    mlir::Value zero_point = getOptionalOperand(op, 2);
+    auto resultType =
+        mlir::dyn_cast<mlir::RankedTensorType>(op->getResult(0).getType());
     if (!resultType)
       return rewriter.notifyMatchFailure(op, "expected ranked output");
     mlir::Value init = createEmptyTensor(rewriter, loc, resultType, input);
 
     auto loc = op->getLoc();
-  llvm::SmallVector<mlir::Value> operands = {context, input, scale};
-if(zero_point)
-    operands.push_back(zero_point);
-    auto quantizeLinearOp = mlir::hip::QuantizeLinearOp::create(rewriter, loc, operands);
+    llvm::SmallVector<mlir::Value> operands = {context, input, scale};
+    if (zero_point)
+      operands.push_back(zero_point);
+    auto quantizeLinearOp =
+        mlir::hip::QuantizeLinearOp::create(rewriter, loc, operands);
     rewriter.replaceOp(op, quantizeLinearOp.getResult());
     return success();
-}
+  }
 };
 struct DequantizeLinearDecompose : public mlir::RewritePattern {
-    DequantizeLinearDecompose(mlir::MLIRContext *ctx)
-    : RewritePattern("onnx.DequantizeLinear", /*benefit=*/1, ctx) {}
+  DequantizeLinearDecompose(mlir::MLIRContext *ctx)
+      : RewritePattern("onnx.DequantizeLinear", /*benefit=*/1, ctx) {}
 
-    mlir::LogicalResult
+  mlir::LogicalResult
   matchAndRewrite(mlir::Operation *op,
                   mlir::PatternRewriter &rewriter) const override {
 
-                    return success();
-    }
+    return success();
+  }
 };
 
-} // namespace {}
+} // namespace
 
 void populateQuantizeLinearConversionPatterns(RewritePatternSet &patterns,
-  MLIRContext *ctx) {
+                                              MLIRContext *ctx) {
   patterns.add<QuantizeLinearDecompose>(ctx);
 }
 
 void populateDequantizeLinearConversionPatterns(RewritePatternSet &patterns,
-  MLIRContext *ctx) {
+                                                MLIRContext *ctx) {
   patterns.add<DequantizeLinearDecompose>(ctx);
 }
-} // namespace mlir
 } // namespace hip
+} // namespace mlir
