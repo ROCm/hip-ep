55a15c141ad33634220a49449c79feba5bebef25
Wang Chunye <chunywan@xilinx.com>
2026-09-06 14:11:46 +0000

    fix: Add license headers and revert CMake version changes
    - Add AMD copyright headers to new CMake and C++ files (pre-commit fix)
    - Revert morphizen/CMakeLists.txt: restore cmake_minimum_required(3.27)
    - Revert backend-mlir-compiler/CMakeLists.txt: restore cmake_minimum_required(3.29)
    
    CMake version downgrades were temporary workarounds for local build testing
    and should not be part of the PR. The Chez Scheme integration itself does
    not require any CMake version changes.

--- lib/Dialect/Hipsr/Scheme/SchemeRuntime.cpp
+++ lib/Dialect/Hipsr/Scheme/SchemeRuntime.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 //===- SchemeRuntime.cpp - Chez Scheme Runtime Initialization ---*- C++ -*-===//
 //
 // Initializes Chez Scheme with embedded boot file containing rime
@@ -9,12 +14,12 @@
 
 // Chez Scheme C API (minimal declarations)
 extern "C" {
-void Sscheme_init(void *(*)(size_t), void (*)(void*));
-void Sregister_boot_file_bytes(const char*, const unsigned char*, size_t);
-void Sbuild_heap(const char*, void (*)(const char*));
-void* Scons(void*, void*);
-void* Sstring_to_symbol(const char*);
-void* Stop_level_value(void*);
+void Sscheme_init(void *(*)(size_t), void (*)(void *));
+void Sregister_boot_file_bytes(const char *, const unsigned char *, size_t);
+void Sbuild_heap(const char *, void (*)(const char *));
+void *Scons(void *, void *);
+void *Sstring_to_symbol(const char *);
+void *Stop_level_value(void *);
 
 // Embedded boot file
 extern const unsigned char chez_boot_data[];
@@ -33,14 +38,15 @@ bool initializeSchemeRuntime() {
   if (scheme_initialized)
     return true;
 
-  llvm::errs() << "Chez Scheme integration placeholder (boot file embedded)...\n";
+  llvm::errs()
+      << "Chez Scheme integration placeholder (boot file embedded)...\n";
   llvm::errs() << "Boot size: " << chez_boot_size << " bytes\n";
 
   // TODO: Initialize Chez with embedded boot file
   // This requires linking with Chez Scheme kernel library
   // Sscheme_init(nullptr, nullptr);
-  // Sregister_boot_file_bytes("hip-patterns.boot", chez_boot_data, chez_boot_size);
-  // Sbuild_heap(nullptr, nullptr);
+  // Sregister_boot_file_bytes("hip-patterns.boot", chez_boot_data,
+  // chez_boot_size); Sbuild_heap(nullptr, nullptr);
 
   llvm::errs() << "Placeholder initialization complete.\n";
 
@@ -49,7 +55,7 @@ bool initializeSchemeRuntime() {
 }
 
 /// Evaluate Scheme expression (simple wrapper)
-void* evalSchemeString(const char* expr) {
+void *evalSchemeString(const char *expr) {
   // TODO: Implement using Seval_string or similar
   return nullptr;
 }
