ede8fdec9ca0277c99a93a9cfeeb545f40e94d5a
Ilango Rajagopal <ilango.rajagopal@amd.com>
2026-09-07 23:16:04 -0600

    Add all the pointwise ops

--- lib/Dialect/Transforms/FuseROCMlir.cpp
+++ lib/Dialect/Transforms/FuseROCMlir.cpp
@@ -107,7 +107,12 @@ public:
   }
 
   bool isaPointwiseOp(Operation *op) const {
-    return isa_and_present<AddOp, MaxOp>(op);
+    return isa_and_present<MulOp, AddOp, MinOp, MaxOp, SiluOp, SigmoidOp,
+                           TanhOp, SoftplusOp, GeluOp, BiasGeluOp, FastGeluOp,
+                           LeakyReluOp, ReciprocalOp, SqrtOp, DivOp, EqualOp,
+                           AndOp, OrOp, NotOp, CosOp, ErfOp, SinOp, CeilOp,
+                           RoundOp, AtanOp, FloorOp, ExpOp, LogOp, AbsOp, NegOp,
+                           SubOp, CastOp, LessOp, SignOp, ModOp>(op);
   }
 
 private:
