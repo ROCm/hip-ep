f52f372f5abc516c6959c3154564cdb03ba1c5ac
Wang Chunye <chunywan@xilinx.com>
2026-09-23 07:58:56 +0000

    Format: Add comments to dag-chain-2ops codegen expectations
    Added inline comments explaining each step of the generated pattern matching:
    - Root result binding
    - Backwards DAG navigation (root -> intermediate -> leaf)
    - Operation name and result count checks
    - Operand bindings
    - Rewrite placeholder
    
    Makes the generated code easier to understand for human readers.

--- lib/Dialect/Hipsr/Scheme/test/cases/dag-chain-2ops.scm
+++ lib/Dialect/Hipsr/Scheme/test/cases/dag-chain-2ops.scm
@@ -13,36 +13,41 @@
              [%a (make-unbound-value)]
              [%b (make-unbound-value)]
              [all-operations (make-vector 2 (make-unbound-value))])
+
+         ;; Bind root result: %b = result 0 of root operation
          (set! %b (mlir-operation-get-result op 0))
-         (if (and (let ([def-op (mlir-value-get-defining-op %b)])
-                    (and def-op
-                         (begin (vector-set! all-operations 1 def-op) #t)))
-                  (and (string=?
-                         (mlir-operation-name (vector-ref all-operations 1))
-                         "op2")
-                       (= (mlir-operation-num-results
-                            (vector-ref all-operations 1))
-                          1))
-                  (begin
-                    (set! %a
-                      (mlir-operation-get-operand-value
-                        (vector-ref all-operations 1)
-                        0))
-                    #t)
-                  (let ([def-op (mlir-value-get-defining-op %a)])
-                    (and def-op
-                         (begin (vector-set! all-operations 0 def-op) #t)))
-                  (and (string=?
-                         (mlir-operation-name (vector-ref all-operations 0))
-                         "op1")
-                       (= (mlir-operation-num-results
-                            (vector-ref all-operations 0))
-                          1))
-                  (begin
-                    (set! %x
-                      (mlir-operation-get-operand-value
-                        (vector-ref all-operations 0)
-                        0))
-                    #t))
+
+         (if (and
+               ;; Navigate: %b -> defining op (op2) and store in all-operations[1]
+               (let ([def-op (mlir-value-get-defining-op %b)])
+                 (and def-op
+                      (begin (vector-set! all-operations 1 def-op) #t)))
+
+               ;; Check: op2 name and result count
+               (and (string=? (mlir-operation-name (vector-ref all-operations 1)) "op2")
+                    (= (mlir-operation-num-results (vector-ref all-operations 1)) 1))
+
+               ;; Bind: %a = operand 0 of op2
+               (begin
+                 (set! %a (mlir-operation-get-operand-value
+                           (vector-ref all-operations 1) 0))
+                 #t)
+
+               ;; Navigate: %a -> defining op (op1) and store in all-operations[0]
+               (let ([def-op (mlir-value-get-defining-op %a)])
+                 (and def-op
+                      (begin (vector-set! all-operations 0 def-op) #t)))
+
+               ;; Check: op1 name and result count
+               (and (string=? (mlir-operation-name (vector-ref all-operations 0)) "op1")
+                    (= (mlir-operation-num-results (vector-ref all-operations 0)) 1))
+
+               ;; Bind: %x = operand 0 of op1
+               (begin
+                 (set! %x (mlir-operation-get-operand-value
+                           (vector-ref all-operations 0) 0))
+                 #t))
+
+             ;; Rewrite: TODO - not implemented yet
              (error 'todo "rewrite not implemented yet")
              #f)))))
