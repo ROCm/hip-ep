d9957b9d7dbf0fa3867d3109b158f8318e8eb59d
Pahari <tuhinp@amd.com>
2026-09-24 05:36:39 -0600

    Revert "feat(fuse-rocmlir): promote rank-0 kernel operands to tensor<1xT> (#1108)"
    This reverts commit 6b18e139efccd2acfb10e9603744d54bfbc5257b.

--- lib/Dialect/Transforms/FuseROCMlir.cpp
+++ lib/Dialect/Transforms/FuseROCMlir.cpp
@@ -9,9 +9,7 @@
 #include <llvm/ADT/SmallVectorExtras.h>
 #include <llvm/Support/Debug.h>
 #include <mlir/Analysis/TopologicalSortUtils.h>
-#include <mlir/Dialect/Arith/IR/Arith.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
-#include <mlir/Dialect/Tensor/IR/Tensor.h>
 #include <mlir/Dialect/UB/IR/UBOps.h>
 #include <mlir/IR/BuiltinAttributes.h>
 #include <mlir/IR/BuiltinOps.h>
@@ -122,15 +120,7 @@ public:
       PatternRewriter::InsertionGuard guard(rewriter);
       rewriter.setInsertionPointToStart(parentModule.getBody());
       auto funcType = rewriter.getFunctionType(
-          llvm::map_to_vector(
-              operands,
-              [](Value v) -> Type {
-                auto type = dyn_cast<RankedTensorType>(v.getType());
-                if (type && type.getRank() == 0)
-                  return RankedTensorType::get({1}, type.getElementType(),
-                                               type.getEncoding());
-                return v.getType();
-              }),
+          llvm::map_to_vector(operands, [](Value v) { return v.getType(); }),
           endOp->getResultTypes());
 
       newFunc = func::FuncOp::create(rewriter, rewriter.getUnknownLoc(),
@@ -163,36 +153,10 @@ public:
     }
 
     rewriter.setInsertionPointAfter(endOp);
-    SmallVector<Value> dispatchOperands;
-    dispatchOperands.reserve(operands.size());
-    Value rankOneShape;
-    for (Value operand : operands) {
-      auto type = dyn_cast<RankedTensorType>(operand.getType());
-      if (!type || type.getRank() != 0) {
-        dispatchOperands.push_back(operand);
-        continue;
-      }
-
-      // rocMLIR's MIGraphX path represents scalar elementwise operands as
-      // one-element tensors. Match that ABI at the outlining boundary instead
-      // of passing a rank-0 buffer into rock.transforms_to_ptr: a scalar has no
-      // coordinate to linearize, while tensor<1xT> has the expected offset 0.
-      if (!rankOneShape) {
-        auto shapeType = RankedTensorType::get({1}, rewriter.getIndexType());
-        auto shapeAttr =
-            DenseIntElementsAttr::get(shapeType, ArrayRef<int64_t>{1});
-        rankOneShape = arith::ConstantOp::create(
-            rewriter, rewriter.getUnknownLoc(), shapeType, shapeAttr);
-      }
-      auto rankOneType =
-          RankedTensorType::get({1}, type.getElementType(), type.getEncoding());
-      dispatchOperands.push_back(
-          tensor::ReshapeOp::create(rewriter, rewriter.getUnknownLoc(),
-                                    rankOneType, operand, rankOneShape));
-    }
     auto rocMlirOp = RocMlirOp::create(
         rewriter, rewriter.getUnknownLoc(), endOp->getResultTypes(),
-        SymbolRefAttr::get(newFunc), context, dispatchOperands,
+        SymbolRefAttr::get(newFunc), context,
+        SmallVector<Value>(operands.begin(), operands.end()),
         endOp.getDpsInits().front());
 
     rewriter.replaceOp(endOp, rocMlirOp);
